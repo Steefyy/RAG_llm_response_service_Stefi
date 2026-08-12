d04f52f019b9c4bfc6f4aafdff43b408ae35ddea
Boteanu Ștefania Roxana <boteanustefania@yahoo.com>
2026-08-12 10:54:50 +0300

    Curatare test_post_chat.py: eliminare referinte colocviale si la specificatiile temei

--- test_post_chat.py
+++ test_post_chat.py
@@ -1,10 +1,10 @@
 import requests
 import json
 
-# URL-ul serviciului tau de Chat (Persoana A - Port 8000)
+# URL-ul serviciului de Chat
 url = "http://localhost:8000/chat"
 
-# Payload-ul exact al cererii (conform contractului cu Monolitul Java)
+# Payload-ul cererii de testare
 payload = {
     "intrebare": "Care este tema cursului si ce subiecte se discuta?",
     "studentId": 101,
@@ -16,7 +16,7 @@ headers = {
     "Content-Type": "application/json"
 }
 
-print("[+] Trimitere cerere POST /chat catre Docker...")
+print("[+] Trimitere cerere POST /chat...")
 try:
     response = requests.post(url, json=payload, headers=headers)
     print(f"Status Code: {response.status_code}\n")
